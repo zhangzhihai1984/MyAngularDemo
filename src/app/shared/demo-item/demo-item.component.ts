6af6f904e4ab78fe7dda2f817cab6a01a2ecc5e8
zhangzhihai <13504984520@139.com>
2019-02-19 16:28:14 +0800

    Demo Item

--- src/app/shared/demo-item/demo-item.component.ts
+++ src/app/shared/demo-item/demo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-demo-item',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demo-item.component.scss']
 })
 export class DemoItemComponent implements OnInit {
+
+  @Input() title: string;
 
   constructor() { }
 
