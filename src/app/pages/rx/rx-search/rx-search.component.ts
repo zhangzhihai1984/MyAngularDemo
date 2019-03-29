fc1ff99b7f291ec0f886f6e32ac4fef2b73d9220
zhangzhihai <13504984520@139.com>
2019-03-29 15:54:13 +0800

    rx demo search

---
+++ src/app/pages/rx/rx-search/rx-search.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Subject, fromEvent, from, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, switchMap, filter, reduce, startWith } from 'rxjs/operators';
+import { city_data } from './address.data';
+
+@Component({
+  selector: 'app-rx-search',
+  templateUrl: './rx-search.component.html',
+  styleUrls: ['./rx-search.component.scss']
+})
+export class RxSearchComponent implements OnInit {
+
+  @Input() stopSubject: Subject<any>;
+  @Output() logAdded = new EventEmitter<any>();
+
+  @ViewChild('inputRef', { read: ElementRef }) inputRef: ElementRef;
+
+  activated = false;
+
+  $searchResults: Observable<string[]>;
+  cities = this.getCities();
+
+  constructor() { }
+
+  ngOnInit() {
+    this.activated = true;
+
+    this.$searchResults = fromEvent(this.inputRef.nativeElement, 'keyup').pipe(
+      debounceTime(500),
+      map((ev: any) => ev.target.value.trim()),
+      distinctUntilChanged(),
+      switchMap(v => from(this.cities).pipe(
+        filter(city => city.includes(v)),
+        reduce((acc: string[], curr: string) => [...acc, curr], [])
+      )),
+      startWith(this.cities)
+    )
+  }
+
+  getCities(): string[] {
+    const cities = [];
+    for (const provinceName in city_data) {
+      for (const cityName in city_data[provinceName]) {
+        cities.push(cityName);
+      }
+    }
+
+    return cities;
+  }
+}
--- src/app/pages/rx/rx.module.ts
+++ src/app/pages/rx/rx.module.ts
@@ -7,13 +7,15 @@ import { SharedModule } from 'src/app/shared';
 import { RxSumComponent } from './rx-sum/rx-sum.component';
 import { RxExitComponent } from './rx-exit/rx-exit.component';
 import { RxCounterComponent } from './rx-counter/rx-counter.component';
+import { RxSearchComponent } from './rx-search/rx-search.component';
 
 @NgModule({
   declarations: [
     RxListComponent,
     RxSumComponent,
     RxExitComponent,
-    RxCounterComponent
+    RxCounterComponent,
+    RxSearchComponent
   ],
   imports: [
     CommonModule,
