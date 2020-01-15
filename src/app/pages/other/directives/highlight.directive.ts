47f1aacb6951f033f922c8216c9cf99ece28c189
zhangzhihai <13504984520@139.com>
2020-01-15 17:01:42 +0800

    directive demo

--- src/app/pages/other/directives/highlight.directive.ts
+++ src/app/pages/other/directives/highlight.directive.ts
@@ -1,12 +1,16 @@
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, HostBinding, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
+  // selector: 'div[highlight]' only div
+  // selector: 'div[highlight],p[highlight]' only div and p
+  // selector: ':not(a)[highlight]' except a
+  // selector: ':not(a):not(area)[highlight]' except a and area
 })
 export class HighlightDirective {
   @Input('highlight') highlightColor: string
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef, private render: Renderer2) { }
 
   @HostListener('mouseenter') onMouseEnter() {
     this.highlight(this.highlightColor || 'red')
@@ -16,8 +20,14 @@ export class HighlightDirective {
     this.highlight(null)
   }
 
+  @HostBinding('style.border') border: string
+  @HostBinding('style.font-size') fontSize: string
+
   private highlight(color: string) {
-    this.el.nativeElement.style.backgroundColor = color
+    // this.el.nativeElement.style.color = color
+    this.render.setStyle(this.el.nativeElement, 'color', color)
+    this.border = color ? `2px solid ${color}` : null
+    this.fontSize = color ? '150%' : '100%'
   }
 
   showLog = () => console.log("Higlight showLog")
