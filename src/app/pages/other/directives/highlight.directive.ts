4a1e4bd4f0b318879a47c80009a3ed00e7741a3e
zhangzhihai <13504984520@139.com>
2020-01-16 14:18:30 +0800

    directive demo

--- src/app/pages/other/directives/highlight.directive.ts
+++ src/app/pages/other/directives/highlight.directive.ts
@@ -1,11 +1,16 @@
 import { Directive, ElementRef, HostListener, Input, HostBinding, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[highlight]'
+  selector: '[highlight]',
   // selector: 'div[highlight]' only div
   // selector: 'div[highlight],p[highlight]' only div and p
   // selector: ':not(a)[highlight]' except a
   // selector: ':not(a):not(area)[highlight]' except a and area
+  //https://github.com/angular/angular/blob/master/packages/router/src/directives/router_link.ts
+
+  exportAs: 'highlight'
+  //https://github.com/angular/angular/blob/master/packages/router/src/directives/router_outlet.ts
+  //https://netbasal.com/angular-2-take-advantage-of-the-exportas-property-81374ce24d26
 })
 export class HighlightDirective {
   @Input('highlight') highlightColor: string
@@ -31,4 +36,5 @@ export class HighlightDirective {
   }
 
   showLog = () => console.log("Higlight showLog")
+  showAlert = () => window.alert('Directive exportAs works')
 }
