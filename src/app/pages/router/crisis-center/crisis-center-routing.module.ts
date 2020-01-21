a6db005117cb7349039c2d1c27aa3e808eed34c2
zhangzhihai <13504984520@139.com>
2020-01-21 14:57:05 +0800

    router demo

--- src/app/pages/router/crisis-center/crisis-center-routing.module.ts
+++ src/app/pages/router/crisis-center/crisis-center-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { CrisisDetailComponent } from './crisis-detail/crisis-detail.component';
 
 
 const routes: Routes = [
@@ -11,7 +12,13 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        component: CrisisListComponent
+        component: CrisisListComponent,
+        children: [
+          {
+            path: ':id',
+            component: CrisisDetailComponent
+          }
+        ]
       }
     ]
   }
--- src/app/pages/router/crisis-center/crisis-center.module.ts
+++ src/app/pages/router/crisis-center/crisis-center.module.ts
@@ -4,12 +4,14 @@ import { CrisisCenterRoutingModule } from './crisis-center-routing.module';
 import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
 import { SharedModule } from 'src/app/shared';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { CrisisDetailComponent } from './crisis-detail/crisis-detail.component';
 
 
 @NgModule({
   declarations: [
     CrisisCenterComponent,
-    CrisisListComponent
+    CrisisListComponent,
+    CrisisDetailComponent
   ],
   imports: [
     SharedModule,
