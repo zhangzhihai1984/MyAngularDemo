04e23f29f0290ca24858a74261e724edc803472c
zhangzhihai <13504984520@139.com>
2020-01-21 13:28:12 +0800

    router demo

--- src/app/pages/router/crisis-center/crisis-center-routing.module.ts
+++ src/app/pages/router/crisis-center/crisis-center-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
-import { CrisisListComponent } from '../crisis-list/crisis-list.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
 
 
 const routes: Routes = [
--- src/app/pages/router/crisis-center/crisis-center.module.ts
+++ src/app/pages/router/crisis-center/crisis-center.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { CrisisCenterRoutingModule } from './crisis-center-routing.module';
 import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
 import { SharedModule } from 'src/app/shared';
-import { CrisisListComponent } from '../crisis-list/crisis-list.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
 
 
 @NgModule({
