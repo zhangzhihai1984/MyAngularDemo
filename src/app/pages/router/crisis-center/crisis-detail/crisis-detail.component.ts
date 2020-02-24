7dc0c46cc77740f824157c2b08a4e298df0e9927
zhangzhihai <13504984520@139.com>
2020-02-24 12:10:47 +0800

    router demo

--- src/app/pages/router/crisis-center/crisis-detail/crisis-detail.component.ts
+++ src/app/pages/router/crisis-center/crisis-detail/crisis-detail.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Crisis } from '../crisis';
-import { CrisisService } from '../crisis.service';
-import { Router, ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-crisis-detail',
@@ -11,20 +8,17 @@ import { switchMap } from 'rxjs/operators';
   styleUrls: ['./crisis-detail.component.scss']
 })
 export class CrisisDetailComponent implements OnInit {
-  crisis$: Observable<Crisis>
+  crisis: Crisis
+  editName: string
 
   constructor(
-    private service: CrisisService,
-    private router: Router,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
-    this.route.data.subscribe(data => console.log(`data ${JSON.stringify(data)}`)
-    )
-    this.crisis$ = this.route.paramMap.pipe(
-      switchMap(param => this.service.getCrisis(param.get('id')))
-    )
+    this.route.data.subscribe((data: { crisis: Crisis }) => {
+      this.crisis = data.crisis
+      this.editName = data.crisis.name
+    })
   }
-
 }
