e00385634226b1222398043ae26d98c540900113
zhangzhihai <13504984520@139.com>
2020-02-26 15:03:59 +0800

    router demo

--- src/app/pages/router/admin/admin-routing.module.ts
+++ src/app/pages/router/admin/admin-routing.module.ts
@@ -4,12 +4,14 @@ import { AdminComponent } from './admin/admin.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { ManageCrisesComponent } from './manage-crises/manage-crises.component';
 import { ManageHeroesComponent } from './manage-heroes/manage-heroes.component';
+import { AuthGuard } from '../auth/auth.guard';
 
 
 const routes: Routes = [
   {
     path: '',
     component: AdminComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: '',
---
+++ src/app/pages/router/auth/auth.guard.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanActivateChild, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
+  constructor(private authService: AuthService, private router: Router) { }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    return this.checkLogin(state.url);
+  }
+  canActivateChild(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    return true;
+  }
+
+  checkLogin(url: string): boolean {
+    if (this.authService.isLoggedIn)
+      return true
+
+    this.authService.redirectUrl = url
+    // console.log(`<> ${url.substring(0, url.lastIndexOf('/'))} /login`);
+    // console.log(`<> ${url.replace('admin', 'login')}`)
+
+    this.router.navigateByUrl(url.replace('admin', 'login'))
+    return false
+  }
+}
