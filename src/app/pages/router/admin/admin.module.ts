da7caa7bd2a583f456754d6c6e7b5a4a7de95a5d
zhangzhihai <13504984520@139.com>
2020-02-26 12:31:50 +0800

    router demo

--- src/app/pages/router/admin/admin.module.ts
+++ src/app/pages/router/admin/admin.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin/admin.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { ManageHeroesComponent } from './manage-heroes/manage-heroes.component';
+import { ManageCrisesComponent } from './manage-crises/manage-crises.component';
 
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [
+    AdminComponent,
+    AdminDashboardComponent,
+    ManageHeroesComponent,
+    ManageCrisesComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
