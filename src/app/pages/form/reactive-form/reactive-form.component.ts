3be4cb64468eeff5b376ac7c60de6c35b725fd0b
zhangzhihai <13504984520@139.com>
2020-03-09 17:51:12 +0800

    form demo

--- src/app/pages/form/reactive-form/reactive-form.component.ts
+++ src/app/pages/form/reactive-form/reactive-form.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.scss']
+  styleUrls: ['./reactive-form.component.scss'],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: "outline" } }
+  ]
 })
 export class ReactiveFormComponent implements OnInit {
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl('')
+  })
 
   constructor() { }
 
   ngOnInit(): void {
+    this.profileForm.valueChanges.subscribe(v => console.log(`${JSON.stringify(v)}`))
   }
 
+  onSubmit() {
+    console.log(`<> ${this.profileForm.valid}`)
+  }
 }
