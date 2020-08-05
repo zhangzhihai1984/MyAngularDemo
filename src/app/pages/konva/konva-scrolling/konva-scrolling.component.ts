cfbe91bbacc4894aae0e5463fe7b5174754055f7
zhangzhihai <13504984520@139.com>
2020-08-05 14:59:01 +0800

    konva scrolling

---
+++ src/app/pages/konva/konva-scrolling/konva-scrolling.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-konva-scrolling',
+  templateUrl: './konva-scrolling.component.html',
+  styleUrls: ['./konva-scrolling.component.scss']
+})
+export class KonvaScrollingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/konva/konva.module.ts
+++ src/app/pages/konva/konva.module.ts
@@ -4,11 +4,13 @@ import { KonvaRoutingModule } from './konva-routing.module';
 import { Konva1Component } from './konva1/konva1.component';
 import { SharedModule } from 'src/app/shared';
 import { KonvaListComponent } from './konva-list/konva-list.component';
+import { KonvaScrollingComponent } from './konva-scrolling/konva-scrolling.component';
 
 @NgModule({
   declarations: [
     KonvaListComponent,
-    Konva1Component
+    Konva1Component,
+    KonvaScrollingComponent
   ],
   imports: [
     SharedModule,
