1e19d22f549c746182d8104f0ff8c8ccf1152d01
zhangzhihai <13504984520@139.com>
2020-08-05 17:11:31 +0800

    konva scrolling

--- src/app/pages/konva/konva-scrolling/konva-scrolling.component.ts
+++ src/app/pages/konva/konva-scrolling/konva-scrolling.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Konva from 'konva';
 
 @Component({
   selector: 'app-konva-scrolling',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./konva-scrolling.component.scss']
 })
 export class KonvaScrollingComponent implements OnInit {
+  private WIDTH = 2000
+  private HEIGHT = 500
+  private COUNT = 200
 
   constructor() { }
 
   ngOnInit(): void {
+    const stage = new Konva.Stage({
+      container: 'container',
+      width: this.WIDTH,
+      height: this.HEIGHT
+    })
+
+    const layer = new Konva.Layer()
+
+    stage.add(layer)
+    Array.from(Array(this.COUNT).keys()).forEach(_ => layer.add(this.makeNode()))
+    layer.draw()
   }
 
+  private makeNode = (): Konva.Circle =>
+    new Konva.Circle({
+      x: this.WIDTH * Math.random(),
+      y: this.HEIGHT * Math.random(),
+      radius: 50,
+      fill: 'red',
+      stroke: 'black'
+    })
 }
