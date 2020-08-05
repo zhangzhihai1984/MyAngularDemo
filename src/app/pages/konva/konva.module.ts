14c9bcf0123e79fef7c4b530e2cc11b35955c8f4
zhangzhihai <13504984520@139.com>
2020-08-05 17:19:27 +0800

    konva

--- src/app/pages/konva/konva.module.ts
+++ src/app/pages/konva/konva.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 
 import { KonvaRoutingModule } from './konva-routing.module';
-import { Konva1Component } from './konva1/konva1.component';
 import { SharedModule } from 'src/app/shared';
 import { KonvaListComponent } from './konva-list/konva-list.component';
 import { KonvaScrollingComponent } from './konva-scrolling/konva-scrolling.component';
@@ -9,7 +8,6 @@ import { KonvaScrollingComponent } from './konva-scrolling/konva-scrolling.compo
 @NgModule({
   declarations: [
     KonvaListComponent,
-    Konva1Component,
     KonvaScrollingComponent
   ],
   imports: [
