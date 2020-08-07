f1cca7656d95716c113588e67a43a0bae09b683a
zhangzhihai <13504984520@139.com>
2020-08-07 13:45:20 +0800

    konva circuit

---
+++ src/app/pages/konva/konva-circuit/konva-circuit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-konva-circuit',
+  templateUrl: './konva-circuit.component.html',
+  styleUrls: ['./konva-circuit.component.scss']
+})
+export class KonvaCircuitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/konva/konva.module.ts
+++ src/app/pages/konva/konva.module.ts
@@ -4,11 +4,13 @@ import { KonvaRoutingModule } from './konva-routing.module';
 import { SharedModule } from 'src/app/shared';
 import { KonvaListComponent } from './konva-list/konva-list.component';
 import { KonvaScrollingComponent } from './konva-scrolling/konva-scrolling.component';
+import { KonvaCircuitComponent } from './konva-circuit/konva-circuit.component';
 
 @NgModule({
   declarations: [
     KonvaListComponent,
-    KonvaScrollingComponent
+    KonvaScrollingComponent,
+    KonvaCircuitComponent
   ],
   imports: [
     SharedModule,
